d15f3a371e8d735cad3990a3e1bd83777d448746
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-09-07 10:17:22 +0300

    div text

--- js/i.js
+++ js/i.js
@@ -19,6 +19,22 @@ const createDiv = () => document.createElement('DIV');
 // // supaprastintas uzrasymas, galima naudoti tada kai yra viena eilute funkcijoje
 
 
+// sukurto elemto div idejimas i html div'a
+const root = document.querySelector('#root') // surastas elementas su id
+root.appendChild(createDiv()); // i elementa idedamas naujas sukuratas elementas (div'as)
+
+//  funkcija su string'o argumentu, kuris patampa tekstu sukurto div'o viduje
+const createDivText = (text) => {
+    const element =  document.createElement('DIV');
+    const textNode = document.createTextNode(text);
+    element.appendChild(textNode);
+    return element;
+}
+
+root.appendChild(createDivText('Valio')); // idedmas tekstas i div'a
+
+
+
 
 
 const animals = ['Kiškis', 'Lapė', 'Anakonda', 'Sausumų vėplys', 'Bebras'];
