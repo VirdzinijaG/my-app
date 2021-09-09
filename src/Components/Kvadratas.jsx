fc4dc08e82fe964e6656f0e02b5bdcb4383b4589
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-09-09 18:39:19 +0300

    ...

--- src/Components/Kvadratas.jsx
+++ src/Components/Kvadratas.jsx
@@ -2,6 +2,14 @@ import Bebras from "./Bebras";
 
 // Komponentas
 function Kvadratas(props) {
+    if ('Brigadininkas' === props.bebras) {
+        return (
+            <div className="kvadratas br">
+           <b> <Bebras bebras = {props.bebras}/> </b>
+
+        </div>
+    );
+    }
     return (
         <div className="kvadratas">
             {/* <span>{props.bebras}</span> */}
