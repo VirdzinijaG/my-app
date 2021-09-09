6589074c5af6a8ed22651fd9aea79414ff8b1686
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-09-09 21:32:06 +0300

    state

--- src/Components/Bebras.jsx
+++ src/Components/Bebras.jsx
@@ -1,6 +1,41 @@
-function Bebras(props) {
-    return (
-            <span>{props.bebras}</span>
-    );
+// function Bebras(props) {
+//     return (
+//             <span>{props.bebras}</span>
+//     );
+// }
+
+import React from 'react';
+
+class Bebras extends React.Component {
+    constructor() {
+        super();
+        this.state = { date: new Date() }
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+    componentDidMount() {
+        console.log("Jau", this.props.bebras);
+
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+    componentWillUnmount() {
+    }
+    render() {
+        return (
+            <span>{this.props.bebras}
+                <hr />
+                {this.state.date.toLocaleTimeString()}
+            </span>
+        )
+    }
+
 }
+
 export default Bebras;
