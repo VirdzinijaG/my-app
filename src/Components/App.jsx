1724c46aa4455b7de116469dbe94060972bda387
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-09-09 09:42:20 +0300

    data.map key

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,13 +1,16 @@
 import Kvadratas from './Kvadratas';
 
-function App(props) {
-    return (
-        <>
-           <Kvadratas bebras = {'Rudasis'}/> 
-           <Kvadratas bebras = {'Juodasis'}/> 
-           <Kvadratas bebras = {'Brigadininkas'}/> 
-        </>
-    );
-}
+const data = ['Rudasis', 'Juodasis', 'Brigadininkas']
 
+function App() {
+    return (<>
+        {data.map((b, i) => <Kvadratas key={i} bebras={b} />)}
+    </>); // i masyvo indeksai
+    }
 export default App;
+
+
+// pries tai naudotas return vietoj data
+   {/* <Kvadratas bebras = {'Rudasis'}/> 
+           <Kvadratas bebras = {'Juodasis'}/> 
+           <Kvadratas bebras = {'Brigadininkas'}/>  */}
