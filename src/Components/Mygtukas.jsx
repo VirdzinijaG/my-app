6f4daf14aff13fe1f4a6541913af017c03e58f77
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-09-10 17:22:49 +0300

    event

--- src/Components/Mygtukas.jsx
+++ src/Components/Mygtukas.jsx
@@ -6,27 +6,37 @@ class Mygtukas extends React.Component {
         // this.state = { date: new Date() }
     }
 
-    activateLasers = () => {
+    activateLasers = (e) => {
+        // console.log(e);
+        e.stopPropagation(); // nebespausdina valio
         // console.log('Lazeriai aktyvuoti');
         // console.log(this);
         console.log(`Lazeriai ${this.props.tekstas} aktyvuoti`);
     }
 
+    valio = (e) => {
+        console.log('Valio', this.props.tekstas);
+
+    }
+
+
     componentDidMount() {
 
-        document.querySelector('#root').
-        addEventListener('click', e => {
-            console.log('Valio');
-        })
+        // document.querySelector('#root').
+        //     addEventListener('click', e => {
+        //         console.log('Valio');
+        //     })
 
     }
     componentWillUnmount() {
     }
     render() {
         return (
-            <div className="container" onClick={this.activateLasers}>
-            {/* <div className="container" onClick={ () => this.activateLasers ()}></div> */}
-                <div className="submit-feedback">{this.props.tekstas}</div>
+            // <div className="container" onClick={this.activateLasers}>
+            <div onClick={(e) => this.valio(e)}>
+                <div className="container" onClick={(e) => this.activateLasers(e)}>
+                    <div className="submit-feedback">{this.props.tekstas}</div>
+                </div>
             </div>
         )
     }
