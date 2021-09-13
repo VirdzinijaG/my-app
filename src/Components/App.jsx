314db616850bac5458650f0aa11bfca0d6115b5a
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-09-13 21:37:38 +0300

    button

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,10 +1,9 @@
-import Mygtukas from "./Mygtukas";
-
-const data = [['Spausk', 1], ["Spaudinėk", 10], ['Klikink', 100]]
 
 function App() {
-    return (<>
-        {data.map((b, i) => <Mygtukas key={i} tekstas={b[0]} amount = {b[1]} />)}
-    </>); 
-    }
+    return (
+        <div className='circle'>
+            <button className="container">Jaaa!</button>
+        </div>
+    );
+}
 export default App;
