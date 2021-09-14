47fd7601f3b2319128b67d1d1976f2322a377de2
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-09-14 09:37:29 +0300

    setState

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,9 +1,28 @@
+import React from 'react';
 
-function App() {
-    return (
-        <div className='circle' style={{backgroundColor:'green'}}>
-            <button className="container">Jaaa!</button>
-        </div>
-    );
+class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = { bg: 'green'}
+    }
+
+    changeColor = () => {
+        // this.setState ({
+        //     bg: 'orangered',
+        // }); 
+        // pasikeicia viena karta spalva
+
+        // keiciasi kas kart paspaudus
+        this.setState(state => ({bg: state.bg === 'green' ? 'orangered': 'green'}))
+    }
+
+    render() {
+        return (
+            <div className='circle' style={{ backgroundColor: this.state.bg }}>
+                <button className="container" onClick={this.changeColor}>Jaaa!</button>
+            </div>
+        );
+    }
 }
 export default App;
