4536dfb5df5b3ce792d867f3db5859b6f37517dd
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-09-20 10:39:49 +0300

    ...

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -4,16 +4,32 @@ class App extends React.Component {
 
     constructor() {
         super();
-        this.state = { bg: 'green' }
+        this.state = { sk: '3X' }
     }
-
+    changeNumber = () => {
+        this.setState(state => {
+            let number;
+            if (state.sk === '3X') {
+                number = '6X'
+            }
+            else if (state.sk === '6X') {
+                number = '9X'
+            }
+            else if (state.sk === '9X') {
+                number = '12X'
+            }
+            return (
+                { sk: number }
+            )
+        })
+    }
 
 
     render() {
         return (
             <div>
-                <button>3X</button>
-            </div>
+                <button onClick={this.changeNumber}>{this.state.sk}</button>
+            </div >
         );
     }
 }
