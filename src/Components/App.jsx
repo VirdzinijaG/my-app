859e5f85c18e12f99027f0a235f1ae199f65c2df
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-09-20 11:06:09 +0300

    ...

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,37 +1,11 @@
-import React from 'react';
+import M3 from './M3';
 
-class App extends React.Component {
-
-    constructor() {
-        super();
-        this.state = { sk: '3X' }
-    }
-    changeNumber = () => {
-        this.setState(state => {
-            let number;
-            if (state.sk === '3X') {
-                number = '6X'
-            }
-            else if (state.sk === '6X') {
-                number = '9X'
-            }
-            else if (state.sk === '9X') {
-                number = '12X'
-            }
-            return (
-                { sk: number }
-            )
-        })
-    }
-
-
-    render() {
-        return (
-            <div>
-                <button onClick={this.changeNumber}>{this.state.sk}</button>
-            </div >
-        );
-    }
+function App(props) {
+    return (
+        <>
+            <M3></M3>
+        </>
+    );
 }
 
 export default App;
---
+++ src/Components/M3.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class M3 extends React.Component {
+    constructor(props) {
+        super(props);
+        //   this.state = {date: new Date()};
+    }
+
+    render() {
+        return (
+            <button>3X</button>
+        );
+    }
+}
+
+export default M3;
