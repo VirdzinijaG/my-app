1c713b61517e9a3435517eac7e51ad97b2b166c2
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-09-20 11:23:16 +0300

    localStorage

--- src/Components/M3.jsx
+++ src/Components/M3.jsx
@@ -6,8 +6,21 @@ class M3 extends React.Component {
         this.state = { x: 3 };
     }
     do3 = () => {
-        this.setState((state) => ({ x: state.x += 3 }))
+        localStorage.setItem('xxx', this.state.x + 3);
+        this.setState((state) => ({ x: state.x += 3 }));
     }
+
+    componentDidMount() {
+        const x = localStorage.getItem('xxx');
+        if (null === x) {
+            return;
+        }
+        this.setState({
+            x: parseInt(x)
+        })
+    }
+
+
     render() {
         return (
             <button onClick={this.do3}>{this.state.x}X</button>
