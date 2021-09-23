4a2035ef22fb788d09a8fe956a57bcf27c8e1992
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-09-23 19:27:18 +0300

    ...

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Book from './Book';
 import '../books.css';
+import Sort from './Sort';
 
 function App() {
     const [books, setBooks] = useState([]);
@@ -10,7 +11,20 @@ function App() {
         axios.get('https://in3.dev/knygos/')
             .then(function (response) {
                 console.log(response.data);
-                setBooks(response.data);
+                const books = response.data;
+                books.sort((a, b) => {
+                    if (a.title > b.title) {
+                        return 1;
+                    }
+                    else if (a.title < b.title) {
+                        return -1
+                    }
+                    else {
+                        return 0
+                    }
+                })
+                // setBooks(response.data);
+                setBooks(books)
             })
     }, []);
 
@@ -25,12 +39,61 @@ function App() {
         setBooks(booksCopy);
     }
 
+    const makeSort = (dir) => {
+        console.log(dir);
+        const booksCopy = books.slice()
+        if (dir === 'priceDesc') {
+            booksCopy.sort((a, b) => a.price - b.price);
+            setBooks(booksCopy)
+        }
+        else if (dir === 'priceAsc') {
+            booksCopy.sort((a, b) => b.price - a.price);
+            setBooks(booksCopy)
+        }
+        else if (dir === 'priceAsc') {
+            booksCopy.sort((a, b) => b.price - a.price);
+            setBooks(booksCopy)
+        }
+        else if (dir === 'titleAsc') {
+            booksCopy.sort((a, b) => {
+                if (a.title > b.title) {
+                    return 1;
+                }
+                else if (a.title < b.title) {
+                    return -1;
+                }
+                else {
+                    return 0;
+                }
+            });
+            setBooks(booksCopy);
+        }
+        else if (dir === 'titleDesc') {
+            booksCopy.sort((a, b) => {
+                if (a.title > b.title) {
+                    return -1;
+                }
+                else if (a.title < b.title) {
+                    return 1;
+                }
+                else {
+                    return 0;
+                }
+            });
+            setBooks(booksCopy);
+        }
+    }
+
+
 
     return (
-        <div className="book-container">
+        <>
             <h1>Knygų sąrašas</h1>
-            {books.map((book) => (<Book key={book.id} data={book} delete={sniuriukasBook}></Book>))}
-        </div>
+            <Sort makeSort={makeSort}></Sort>
+            <div className="book-container">
+                {books.map((book) => (<Book key={book.id} data={book} delete={sniuriukasBook}></Book>))}
+            </div>
+        </>
     )
 }
 
