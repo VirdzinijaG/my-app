cdfc8409541c20647c7ff9930127b640e93d03d8
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-10-04 20:28:32 +0300

    09.30

--- src/Constants/types.js
+++ src/Constants/types.js
@@ -1,7 +1,2 @@
 
 // FARM
-export const GET_ALL_FROM_LS = 'GET_ALL_FROM_LS';
-export const ADD_ANIMAL = 'ADD_ANIMAL';
-export const DELETE_ANIMAL = 'DELETE_ANIMAL';
-export const EDIT_ANIMAL = 'EDIT_ANIMAL';
-export const CHANGE_FARM = 'CHANGE_FARM';
