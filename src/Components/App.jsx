9f784031d0ad83652079faf5cb65dd6741d3d032
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-10-06 19:59:19 +0300

    10.06

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,56 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Post from './Post';
-import NewPost from './NewPost';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Post from "./Post";
+import NewPost from "./NewPost";
 
 function App() {
+  const [posts, setPosts] = useState([]);
 
-    const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3003/posts").then(function (response) {
+      console.log(response.data);
+      setPosts(response.data);
+    });
+  }, []);
 
-    useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(function (response) {
-                console.log(response.data);
-                setPosts(response.data);
-            })
-    }, []);
+  const doDelete = (id) => {
+    axios
+      .delete("https://jsonplaceholder.typicode.com/posts/" + id, {})
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
-    const doDelete = id => {
-        axios.delete('https://jsonplaceholder.typicode.com/posts/' + id, {
-        })
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
+  const doAdd = (data) => {
+    axios
+      .post("https://jsonplaceholder.typicode.com/posts/", data)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
-    const doAdd = (data) => {
-        axios.post('https://jsonplaceholder.typicode.com/posts/', data
-        )
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
+  const crud = {
+    delete: doDelete,
+  };
 
-    const crud = {
-        delete: doDelete
-    }
-
-
-    return (
-        <div>
-            <div className="new-post-container">
-                <NewPost add={doAdd} />
-            </div>
-            <div className="posts-container">
-                {posts.map((post) => (<Post key={post.id} data={post} crud={crud} />))}
-            </div>
-        </div>);
+  return (
+    <div>
+      <div className="new-post-container">
+        <NewPost add={doAdd} />
+      </div>
+      <div className="posts-container">
+        {posts.map((post) => (
+          <Post key={post.id} data={post} crud={crud} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
