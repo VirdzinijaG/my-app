444d6fea4df031d822bce90d1b0ae83c8a999358
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-10-07 17:41:12 +0300

    10.07

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,42 +1,53 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import Post from "./Post";
 import NewPost from "./NewPost";
+import PostModal from "./PostModal";
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [postsUpdateTime, setPostsUpdateTime] = useState(Date.now());
+  const [open, setOpen] = useState(0);
+  const modalPost = useRef({ title: "", body: "" });
 
   useEffect(() => {
-    axios.get("http://localhost:3003/posts").then(function (response) {
-      console.log(response.data);
+    axios.get("http://localhost:3003/posts").then((response) => {
       setPosts(response.data);
     });
-  }, []);
+  }, [postsUpdateTime]);
 
   const doDelete = (id) => {
-    axios
-      .delete("https://jsonplaceholder.typicode.com/posts/" + id, {})
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    axios.delete("http://localhost:3003/posts/" + id, {}).then(() => {
+      setPostsUpdateTime(Date.now());
+    });
   };
 
   const doAdd = (data) => {
-    axios
-      .post("http://localhost:3003/posts", data)
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    axios.post("http://localhost:3003/posts", data).then(() => {
+      setPostsUpdateTime(Date.now());
+    });
+  };
+
+  const doEdit = (data) => {
+    axios.put("http://localhost:3003/posts/" + data.id, data).then(() => {
+      setPostsUpdateTime(Date.now());
+    });
+  };
+
+  const openModal = (data) => {
+    modalPost.current = { title: data.title, body: data.body };
+    setOpen(data.id);
+  };
+
+  const closeModal = () => {
+    setOpen(0);
   };
 
   const crud = {
     delete: doDelete,
+    close: closeModal,
+    edit: doEdit,
+    open: openModal,
   };
 
   return (
@@ -49,6 +60,7 @@ function App() {
           <Post key={post.id} data={post} crud={crud} />
         ))}
       </div>
+      <PostModal crud={crud} id={open} data={modalPost.current} />
     </div>
   );
 }
